f510992f4ffa1d26e34207222cfaba5b48eb2bd5
yufuworks <yufuworks@outlook.jp>
2019-04-23 23:17:12 +0900

    MadeFiles
    基本ファイル群の作成
    ・routes => user[:index]追加
    ・users_controller => index追加
    ・_groupform => インクリメンタルサーチ用haml追加、chechbox_tag削除
    ・search.js追加
    ・search.json.jbuilder追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resource :user, only: [:edit, :update]
+  resource :user, only: [:edit, :update, :index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :chats, only: [:index, :create]
   end
