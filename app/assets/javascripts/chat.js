46efe730d6d12f5740a0608c25b6d68b2a062744
yufuworks <yufuworks@outlook.jp>
2019-04-23 19:25:37 +0900

    BuiltAjaxChat
    非同期通信による画面遷移を伴わないチャットコメント更新機能完成
    
    ajaxによってjsonを受け取り後
    jsonデータを含むhtmlを最下部に新たに構築
    最下部へ画面をスクロール

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,7 +1,43 @@
 $(function(){
-  $('.form').on('submit', function(e){
+function buildHTML(data){
+  var html = `<div class="message">
+                <div class="upper-info">
+                  <p class="upper-info__user">${data.name}</p>
+                  <p class="upper-info__date">${data.created_at}</p>
+                </div>
+                <p class="message__text">${data.message}</p>`
+  if (data.image.url == null){
+    html = $(html).append(
+              `</div>`
+  )} else {
+    html = $(html).append(
+                `<img src = '${data.image.url}'>
+              </div>`
+  )}
+  return html;
+}
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
-    var formData = new FormData(this);
+    var formData = new FormData($(this).get(0));
+    $.ajax({
+      url: window.location.href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+      disabled: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#textboxmessage_content').val('')
+      $('.new-message__submit-btn').removeAttr("disabled");
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
 })
---
+++ app/views/chats/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.name @chat.user.name
+json.message  @chat.message
+json.image  @chat.image
+json.user_id  @chat.user_id
+json.group_id  @chat.group_id
+json.created_at @chat.created_at
